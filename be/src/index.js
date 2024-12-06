91aed19e82bf0c8fec0aaefcb6dbb4f4fc207231
Pavan Emani <pavanemani14@gmail.com>
2024-12-06 15:31:32 +0530

    cors

--- be/src/index.js
+++ be/src/index.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-const corsOptions = {
-  origin: 'https://houzzee.vercel.app/', 
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
+// const corsOptions = {
+//   origin: 'https://houzzee.vercel.app/', 
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Connect to MongoDB
 mongoose
