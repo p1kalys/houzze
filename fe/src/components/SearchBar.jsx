8a32b7f78885e3774ab7a4ba200f35a5e722d1e8
Pavan Emani <pavanemani14@gmail.com>
2024-12-06 22:48:08 +0530

    search fix

--- fe/src/components/SearchBar.jsx
+++ fe/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ export const SearchBar = ({ onSearch }) => {
         type="text"
         name='search'
         placeholder="Search by name, city, postcode..."
-        onChange={(e) => onSearch(e.target.value)}
+        onChange={onSearch}
         className="w-full px-4 py-2 pl-10 text-gray-900 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
       />
       <Search className="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
