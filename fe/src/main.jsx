b1ecad93bfade1f802df699805262bcc3892f4e6
Pavan Emani <pavanemani14@gmail.com>
2024-12-06 22:12:50 +0530

    contact condition change

--- fe/src/main.jsx
+++ fe/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 import { BrowserRouter as Router } from "react-router-dom"; 
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
   <Router>
     <App />
+    <Analytics />
   </Router>
   </StrictMode>
 )
