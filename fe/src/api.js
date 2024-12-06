cf7c956ee086b846439748583412282591bd3d6e
Pavan Emani <pavanemani14@gmail.com>
2024-12-06 16:58:45 +0530

    cors

--- fe/src/api.js
+++ fe/src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://houzze-be.onrender.com/api',
-  withCredentials: true,
+  baseURL: 'https://houzze-be.onrender.com/api'
 });
 
 export default API;
